d02fe0dc8e3c1dfb49fd5bdb1c06669a80f6141d
createwithanuj <61941950+createwithanuj@users.noreply.github.com>
2025-06-22 14:09:40 +0530

    Update rate limiter implementation and dependencies; add rate limiting middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import notesRoutes from './routes/notesRoutes.js'
 import dotenv from "dotenv";
 import connectDB from "./config/db.js"
+import rateLimter from './middleware/rateLimiter.js';
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -9,14 +10,18 @@ const PORT = process.env.PORT;
 const app = express();
 
 
-connectDB();
+
 
 app.use(express.json());
 
+app.use(rateLimter);
 
 app.use("/api/notes", notesRoutes);
 
-
-app.listen(PORT || 5001, ()=> {
+connectDB().then(
+    () => {
+        app.listen(PORT || 5001, ()=> {
     console.log('Server is running on port http://localhost:5001');
 })
+    }
+)
